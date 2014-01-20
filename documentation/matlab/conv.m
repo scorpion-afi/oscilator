d2da0a5ffa97eb663b9b1cf367105b07f0ffee45
scorpion_afi <ivan0ivanov0@mail.ru>
2014-01-20 18:03:44 +0200

    matlab files have been corrected.

--- documentation/matlab/conv.m
+++ documentation/matlab/conv.m
@@ -3,8 +3,10 @@ close all;
 clear all;
 fclose( 'all' );
 
+% this is frequency of ADC in Hz
 f_sample = 5000;
 
+% gui open file dialog
 [filename, pathname] = uigetfile( '*.txt', 'Open MeasGeneratorData-file' );
 FILE_NAME = strcat( pathname, filename );
 
@@ -26,63 +28,63 @@ f_id = fopen( FILE_NAME, 'r' );
 % measurement set is set of measured data for 4 channel (8 bytes)
 
 if( f_id > 2 ) 
-  
-  res = fseek( f_id, 0, 'eof' );    % move file pointer to end of file
-  if( res == 0 )
-     
-    file_size = ftell( f_id );      % calculate file size in bytes
-    res = fseek( f_id, 0, 'bof' );  % move file pointer to begin of file
- 
-    % /2, because of file_size keeps size of file in bytes
-    adc_array = zeros( [floor( file_size/2 ), 1] );
-
-    % read data from file to array 'adc_array' (two bytes at time)
+    
+    % read data from file to array 'adc_array' (two bytes at time) (little-endian)
     adc_array = fread( f_id, '*uint16' ); % file will be truncated, if it is necessary
     
+    % adc_array - is column-vector of items (16 bit)
+    
+    % item_num - is amount of full (16 bit) samples
+    [item_num, ~] = size( adc_array );
+  
     % truncation 4 high-order bits (may be not used)
     adc_array( 1:end ) = bitand( adc_array( 1:end ), 4095 );
     
     % create arrays to storing splitted data
     % incomplete measurement set will not be processed,
-    % for example: if file_size = 16...23 only 8 half-words(16 bit) of
-    % adc_array will be processed
-    i_ch1 = zeros( [floor( file_size/8 ), 1] );
-    v_ch1 = zeros( [floor( file_size/8 ), 1] );
-    v_ch2 = zeros( [floor( file_size/8 ), 1] );
-    i_ch2 = zeros( [floor( file_size/8 ), 1] );
-         
+    % for example: if item_num = 8...11 only 8 items (16 bit) of adc_array
+    % will be processed (3-rd measurement set will be drop out) 
+    i_ch1 = zeros( [floor( item_num/4 ), 1] );
+    v_ch1 = zeros( [floor( item_num/4 ), 1] );
+    v_ch2 = zeros( [floor( item_num/4 ), 1] );
+    i_ch2 = zeros( [floor( item_num/4 ), 1] );
+    
+    % i_ch1, v_ch1, v_ch2, i_ch2 - are column-vectors of items (16 bit)
+    
+    % split_size - is size of arrays, which will store splitted data
+    [split_size, ~] = size( i_ch1 );
+    
     % extract data from half-word(16 bit) stream
     % and convert to V and mA
     i_1 = 1;
     i_2 = 2;
     i_3 = 3;
     i_4 = 4;
-
-    for i = 1:size( i_ch1 )       
+      
+    for i = 1:split_size       
       i_ch1( i ) = 0.1943    * double( adc_array( i_1 ) ) - 0.3238;
       v_ch1( i ) = 0.0011848 * double( adc_array( i_2 ) ) - 0.0119;
       v_ch2( i ) = 0.0009407 * double( adc_array( i_3 ) ) - 0.005;
       i_ch2( i ) = 0.1943    * double( adc_array( i_4 ) ) - 0.4829;
-      
+
       i_1 = i_1 + 4;
       i_2 = i_2 + 4;
       i_3 = i_3 + 4;
       i_4 = i_4 + 4;      
     end
-    
+
     % creation time array, for abscissa of plots
-    x = 0:1:size( i_ch1 ) - 1;  % - 1, because of begin index is 0
-    time = x * (1/f_sample);
+    x = 0:1:split_size - 1;  % - 1, because of begin index is 0
+    time = x * ( 1/f_sample );          
     
-    % drawing plots, penultimate parameter is decimation coeficient
-    draw_plot( time, i_ch1, i_ch2, v_ch1, v_ch2, 1, 1 );
-    draw_plot( time, i_ch1, i_ch2, v_ch1, v_ch2, 2, 2 );
-    draw_plot( time, i_ch1, i_ch2, v_ch1, v_ch2, 4, 3 );
-    draw_plot( time, i_ch1, i_ch2, v_ch1, v_ch2, 700, 4 );
+    decimation_coef_array = [ 0 1 3 5 511 ];
     
-  end % if( res == 0 )
-  
-  fclose( f_id );
+    % drawing plots, penultimate parameter is decimation coeficient   
+    parfor i = 1:5
+        draw_plot( time, i_ch1, i_ch2, v_ch1, v_ch2, decimation_coef_array( i ), i ); 
+    end
+          
+    fclose( f_id );
 end % if( f_id > 3 ) 
 
 % clear all;
